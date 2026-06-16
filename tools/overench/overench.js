1bfca260a0d0b936c0ea2587b0010b14a76a9b26
asdf1 <fukiinukai0720@outlook.jp>
2026-06-16 18:18:01 +0900

    Update overench.js

--- tools/overench/overench.js
+++ tools/overench/overench.js
@@ -1,3 +1,15 @@
 function seisei() {
-  alert("Wow!");
+  const id = document.getElementById("id").value;
+  let count;
+  if (["1", ""].includes(document.getElementById("count").value)) {
+    count = "";
+  } else {
+    count = parseInt(document.getElementById("count").value);
+  }
+  
+  let result = '';
+  
+  result += `/give @p ${id} ${count}`;
+
+  document.getElementById('result').textContent = result;
 }
